4fb04a4db2b4d3a8931ad61af764e5ee437d674b
Deepayan Kundu <deepayankundu6@gmail.com>
2023-06-24 10:56:00 +0530

    fixes

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,6 @@ require 'capybara'
 require 'cucumber'
 
 Capybara.default_driver = :selenium
-Capybara.app_host = 'google.co.in'
-Capybara.reguster_driver :selenium do |app|
+Capybara.register_driver :selenium do |app|
     Capybara::selenium::Driver.new app,browser: :chromes
 end
